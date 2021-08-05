ea14ee8a6f864389dfdb0eecba649a5e09aae480
Luke Wilson <wilson.palooza@gmail.com>
2021-08-04 23:33:06 -0700

    initial commit and first steps

---
+++ components/Map.vue
@@ -0,0 +1,87 @@
+<template>
+	<div id="myMap"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl/dist/mapbox-gl.js"
+import airstrikes from "@/data/airstrikes.json"
+mapboxgl.accessToken = process.env.mapboxToken
+
+export default {
+	name: "Map",
+	data: () => ({
+		map: {},
+	}),
+	methods: {
+		addMarkers() {
+			airstrikes.forEach((strike, idx) => {
+				const el = document.createElement("div")
+				el.className = "marker"
+				const marker = new mapboxgl.Marker()
+					.setLngLat([strike.lng, strike.lat])
+					.setPopup(new mapboxgl.Popup().setHTML(this.createPopupHTML(strike)))
+					.addTo(this.map)
+			})
+		},
+		createPopupHTML(strike) {
+			let returnVar = `
+				<h3>${strike.location}</h3>
+				<p>Country: ${strike.country}</p>
+				<p>Date range: ${strike.date}</p>
+				<p>Platform/Weapon: ${strike.platform.WEAPON}</p>
+				<p>Target: ${strike.target}</p>
+				<p>Purpose: ${strike.purpose}</p>
+			`
+			if (strike.youtube_clip.includes("youtube.com")) {
+				returnVar += `<p><a target="_blank" href="${strike.youtube_clip}">Video</a></p>`
+			}
+
+			return returnVar
+		},
+	},
+	mounted() {
+		console.log(airstrikes[3])
+		this.map = new mapboxgl.Map({
+			container: "myMap",
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: {
+				lat: 34.1911,
+				lng: 41.0092,
+			},
+			zoom: 6,
+		})
+		this.map.on("load", () => {
+			this.addMarkers()
+		})
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+#myMap {
+	height: 100vh;
+	width: 100%;
+}
+
+.marker {
+	background-image: url("/static/mapbox-icon.png");
+	background-size: cover;
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	cursor: pointer;
+}
+
+.mapboxgl-popup {
+	max-width: 200px;
+}
+
+.mapboxgl-popup-content {
+	text-align: center;
+	font-family: "Open Sans", sans-serif;
+	background-color: red;
+	h3 {
+		color: purple !important;
+	}
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<Map />
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+	data: () => ({
+		title: "Strike from the Air: First 100 Days",
+	}),
+	head() {
+		return {
+			title: this.title,
+			meta: [
+				// hid is used as unique identifier. Do not use `vmid` for it as it will not work
+				{
+					hid: "description",
+					name: "description",
+					content: "My custom description",
+				},
+			],
+			link: [
+				{
+					href: "https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css",
+					rel: "stylesheet",
+				},
+			],
+		}
+	},
+})
+</script>
